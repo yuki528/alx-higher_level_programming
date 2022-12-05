d5f032f71179eec1a857ef95da64355276a18905
yuki528 <Kiplimomeli44@gmail.com>
2022-12-05 01:33:48 -0800

    commit

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -6,10 +6,10 @@ def divisible_by_2(my_list=[]):
     if len(my_list) is 0:
         return new_list
 
-    for i in my_list:
-        if i % 2 == 0:
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
             new_list.append(True)
         else:
             new_list.append(False)
 
-    return new_list
+    return (new_list)
