8decc3e38eef1664ea2c1a33a5d9c0cadbbdb38e
yuki528 <Kiplimomeli44@gmail.com>
2022-12-07 02:07:30 -0800

    first commit

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    for idx in list(a_dictionary.keys()):
+        if a_dictionary[idx] == value:
+            del a_dictionary[idx]
+    return a_dictionary
+
+
+print_sorted_dictionary = \
+    __import__('6-print_sorted_dictionary').print_sorted_dictionary
+
+a_dictionary = {'lang': "C", 'track': "Low", 'pref': "C", 'ids': [1, 2, 3]}
+new_dict = complex_delete(a_dictionary, 'C')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
+
+print("--")
+print("--")
+new_dict = complex_delete(a_dictionary, 'c_is_fun')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
